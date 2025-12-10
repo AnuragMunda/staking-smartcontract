09c4f0da81fd3fde7cd749759c6d05827aaebcd7
Anurag Munda <anurag.munda005@gmail.com>
2025-12-10 11:20:10 +0000

    refactor

--- programs/staking-smartcontract/src/states/user_stake.rs
+++ programs/staking-smartcontract/src/states/user_stake.rs
@@ -8,9 +8,12 @@ use anchor_lang::prelude::*;
 pub struct UserStake {
     pub user: Pubkey, // The owner of this stake
     pub pool: Pubkey, // The staking pool address
+
     pub staked_balance: u128, // Token amount the user has currently staked
     pub reward_debt: u128, // Rewards already accounted for
+
     pub last_stake_time: i64, // The last time the user changes their stake position
     pub lock_until: i64, // Time period for which the staked is locked
-    pub bump: u8, // The bump for this particular PDA
+
+    pub bump: u8, // Random value to derive user stake pda
 }
