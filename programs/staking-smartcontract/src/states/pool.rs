c22c14ad4d957956a50dcf5c088a3003dba9eee2
Anurag Munda <anurag.munda005@gmail.com>
2025-12-11 18:52:43 +0000

    refactor

--- programs/staking-smartcontract/src/states/pool.rs
+++ programs/staking-smartcontract/src/states/pool.rs
@@ -11,13 +11,14 @@ pub const POOL_SEED: &str = "POOL";
 #[derive(InitSpace)]
 pub struct Pool {
     pub admin: Pubkey, // Admin address of the pool
-    pub staking_mint: Pubkey, // Address of the staking token
+    pub stake_mint: Pubkey, // Address of the staking token
     pub reward_mint: Pubkey, // Address of the reward token
-    pub staking_vault: Pubkey, // Address of the vault for storing stake token
+    pub stake_vault: Pubkey, // Address of the vault for storing stake token
     pub reward_vault: Pubkey, // Address of the vault for storing reward token
 
     pub reward_rate: u64, // Reward token per second
-    pub total_staked: u128, // Total amount staked in the pool
+    pub total_stake: u128, // Total amount staked in the pool
+    pub total_shares: u128, // The sum of all shares minted to all stakers, and it represents 100% of the pool.
 
     pub acc_reward_per_share: u128, // Total accumulated rewards per 1 staked token, stored as a scaled number
     pub last_update_time: i64, // Last timestamp when rewards were calculated
