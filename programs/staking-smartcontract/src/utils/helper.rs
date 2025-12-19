9e9bc32d9a7971915a5558fde293eaa58a07f287
Anurag Munda <anurag.munda005@gmail.com>
2025-12-19 14:20:42 +0000

    add helper functions to helper.rs

---
+++ programs/staking-smartcontract/src/utils/helper.rs
@@ -0,0 +1,53 @@
+use anchor_lang::prelude::*;
+
+use crate::states::{Pool, UserStake};
+use crate::utils::StakingError;
+
+//------------------------------------ Helper Functions / Utils ------------------------------------//
+
+pub const SCALING_FACTOR: u128 = 1_000_000_000_000u128; // 1e12
+
+/// @dev Syncs the reward variables with respect to the elapsed time since last update
+pub fn sync_reward_vars(pool: &mut Account<Pool>, now: i64) -> Result<()> {
+    if now <= pool.last_update_time {
+        return Ok(());
+    }
+
+    // Calculate the time passed since last update
+    let elapsed_time = (now - pool.last_update_time) as u128;
+    if pool.total_shares == 0 || pool.reward_rate == 0 {
+        pool.last_update_time = now;
+        return Ok(());
+    }
+
+    // Calculate new rewards for the elapsed time
+    let new_rewards = (pool.reward_rate as u128)
+        .checked_mul(elapsed_time)
+        .ok_or(StakingError::Overflow)?;
+
+    // Calculate the reward for one share and then add it increment the accumulated reward per share value
+    // reward_per_share += new_rewards * SCALING_FACTOR / total_shares
+    let prod = new_rewards.checked_mul(SCALING_FACTOR).ok_or(StakingError::Overflow)?;
+    let increment = prod.checked_div(pool.total_shares).ok_or(StakingError::Overflow)?;
+
+    pool.acc_reward_per_share = pool.acc_reward_per_share.checked_add(increment).ok_or(StakingError::Overflow)?;
+    pool.last_update_time = now;
+
+    Ok(())
+}
+
+/// @dev Calculates the pending reward to be claimed by a user
+pub fn user_pending_reward(user: &Account<UserStake>, pool: &Account<Pool>) -> Result<u128> {
+    if user.shares == 0 {
+        return Ok(0u128);
+    }
+
+    let prod = user.shares.checked_mul(pool.acc_reward_per_share).ok_or(StakingError::Overflow)?;
+    let acc_reward = prod.checked_div(SCALING_FACTOR).ok_or(StakingError::Overflow)?;
+
+    if acc_reward <= user.reward_debt {
+        return Ok(0u128);
+    }
+
+    Ok(acc_reward.checked_sub(user.reward_debt).ok_or(StakingError::Overflow)?)
+}
--- programs/staking-smartcontract/src/utils/mod.rs
+++ programs/staking-smartcontract/src/utils/mod.rs
@@ -3,3 +3,6 @@ pub use errors::*;
 
 pub mod events;
 pub use events::*;
+
+pub mod helper;
+pub use helper::*;
